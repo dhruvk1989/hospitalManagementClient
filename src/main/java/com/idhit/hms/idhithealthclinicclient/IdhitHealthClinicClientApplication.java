d71284cbd4ab35d8a019028c207964c8755c2747
Dhruv Singh K <dhruvsinghk@Dhruvs-MacBook-Pro.local>
2022-06-23 17:32:06 +0530

    First commit for the client

---
+++ src/main/java/com/idhit/hms/idhithealthclinicclient/IdhitHealthClinicClientApplication.java
@@ -0,0 +1,13 @@
+package com.idhit.hms.idhithealthclinicclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdhitHealthClinicClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdhitHealthClinicClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idhit/hms/idhithealthclinicclient/IdhitHealthClinicClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idhit.hms.idhithealthclinicclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdhitHealthClinicClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
